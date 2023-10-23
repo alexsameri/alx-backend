aa79e334f9d37a3cc44c53abefadb88c7291319e
alexsameri <samerialexx@gmail.com>
2023-10-23 23:31:17 +0300

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+
+'''Module 1
+'''
+import csv
+import math
+from typing import List
+
+
+
+
+class Server:
+    """Server class to paginate a database of popular baby name
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+    
+    def index_range(self, page, page_size):
+        '''Retrieve the index range from a given page and page_size
+        '''
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Use assert to verify arguments age > 0
+        """
+        assert page > 0 and page_size > 0
+        startIndex, endIndex = self.index_range(page, page_size)
+        return self.dataset()[startIndex:endIndex]
