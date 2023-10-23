f728f3b5492f8834d6bdcd7a83914802fa875915
alexsameri <samerialexx@gmail.com>
2023-10-23 22:44:40 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''module one
+'''
+
+
+def index_range(page, page_size):
+    '''Retrieve the index range from a given page and page_size
+    '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
